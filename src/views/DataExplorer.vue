051b7c9fdbc6db121fc2dca8867222e9ad4c16c8
Fleur Kelpin <f.kelpin@umcg.nl>
2021-06-28 14:11:17 +0200

    feat: add data explorer page

---
+++ src/views/DataExplorer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dataexplorer">
+    <h1>{{ id }}</h1>
+    <data-table/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Data from '@/components/Data.vue' // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'DataExplorer',
+  components: { DataTable: Data },
+  props: {
+    id: String
+  }
+})
+</script>
